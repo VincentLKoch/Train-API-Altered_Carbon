7f6db7c8605cb5ad76961f9b51af3cb35d2f1678
VincentLKoch <Vincent.lel.koch@gmail.com>
2019-11-12 10:42:52 +0100

    digitize complete

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 
-import {getClinic} from './weiClinic'
+import { getClinic } from './weiClinic'
 
 const app = express()
 const port = 8081
@@ -15,12 +15,25 @@ app.use(function (_req, res, next) {
 })
 
 app.get('/digitize', (req, res) => {
-    // Retrieve gender, age and name
+    const gender = req.query.gender, name = req.query.name, age = req.query.age
 
-    const createdElements = getClinic().create(gender, name, age)
+    if (gender && name && age) {
+        const createdElements = getClinic().create(gender, name, age)
+        res.status(200).set({ 'Content-Type': 'application/json' }).json(createdElements).end();
+    } else {
+        //force to parse(stringify() to keep the key:null when argument is missing from call (adding replacer)
+        res.status(400).json({
+            message: "missing arguments",
+            receive: {
+                gender: gender,
+                name: name,
+                age: age
+            },
+            needed: ["gender", "name", "age"]
+        }).end();
+    }
+}) //end digitize
 
-    res.status(200).set({ 'Content-Type': 'application/json' }).json(createdElements)
-})
 
 const server = app.listen(port, () => {
     const port = server.address().port
