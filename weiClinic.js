5640e946f882ea975312c876060b4a5f34e0a371
Aadhar-Ag <55687136+Aadhar-Ag@users.noreply.github.com>
2019-11-12 12:04:14 +0100

    Update weiClinic.js

--- weiClinic.js
+++ weiClinic.js
@@ -24,19 +24,63 @@ class WeiClinic {
     }
 
     assignStackToEnvelope(idStack, idEnvelope) {
+        const stack = this.stacks[idStack]
+        const envelope = this.envelopes[idEnvelope] || this.envelopes.find(obj => {return obj.idStack === null})
+
+        if (envelope != undefined){
+            envelope.idStack = stack.id
+            stack.idEnvelope = envelope.id
+            return true
+        }
+        
+       else{ return false}
 
     }
 
     removeStackFromEnvelope(idStack, idEnvelope) {
+        const stack = this.stacks[idStack]
+        const envelope = this.envelopes[idEnvelope] || this.envelopes[stack.idEnvelope]
+
+        if (envelope != undefined){
+            envelope.idStack = null
+            stack.idEnvelope = null
+            return true
+        }
+        
+       else{ return false}
 
     }
 
     killEnvelope(idEnvelope) {
+        const envelope = this.envelopes[idEnvelope]
+        if (envelope != undefined){
 
+            if (envelope.idStack != null) { 
+                const stack = this.stacks[envelope.idStack]
+                stack.idEnvelope = null;
+            }
+            
+            this.envelopes.splice(idEnvelope,1)
+            return true
+         }
+
+        else{ return false}
     }
 
     destroyStack(idStack) {
+        const stack = this.stacks[idStack]
+        if (stack != undefined){
 
+            if (stack.idEnvelope != null) { 
+                const envelope = this.envelopes[stack.idEnvelope]
+                envelope.idStack = null;
+            }
+            
+            this.stacks.splice(idStack,1)
+            return true
+         }
+
+        else{ return false}
     }
 }
 
