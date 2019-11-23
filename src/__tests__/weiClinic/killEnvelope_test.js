478cd2a807229484cd423214b42d022820baa4c0
Aadhar-Ag <55687136+Aadhar-Ag@users.noreply.github.com>
2019-11-23 19:45:02 +0100

    Update killEnvelope_test.js

--- src/__tests__/weiClinic/killEnvelope_test.js
+++ src/__tests__/weiClinic/killEnvelope_test.js
@@ -1,5 +1,8 @@
 import { getClinic } from '../../weiClinic.js'
-
+import CorticalStack from '../../corticalStack.js'
+import Dal from '../../dal.js'
+import CorticalStack from '../../corticalStack'
+import Envelope from '../../Envelope'
 
 describe('Kill test', () => {
     let weiClinic
@@ -10,25 +13,46 @@ describe('Kill test', () => {
     })
 
     it('The enveloppe is killed', () => {
-        weiClinic.envelopes = [{ id: 1, idStack: null }]
+        const fakeEnvelopeMethods = {find(){return new Envelope(1,"M", 8, null)}}
+        Dal.getEnvelopeData  = jest.fn().mockReturnValue(fakeEnvelopeMethods);
+        Dal.getStackData = jest.fn()
+
+        Dal.saveStackData = jest.fn()
+        Dal.removeEnvelopeData = jest.fn()
 
         weiClinic.killEnvelope(1)
 
-        expect(weiClinic.envelopes[0]).toBeUndefined()
+        expect(Dal.getStackData).toHaveBeenCalledTimes(1)
+        expect(Dal.getEnvelopeData).toHaveBeenCalledTimes(1)
+        expect(Dal.saveStackData).toHaveBeenCalledTimes(1)
+        expect(Dal.removeEnvelopeData).toHaveBeenCalledTimes(1)
+        
     });
 
     it('The enveloppe is killed and we keep the stack', () => {
-        weiClinic.envelopes = [{ id: 1, idStack: 1 }]
-        weiClinic.stacks = [{ id: 1, idEnvelope: 1 }]
+        const fakeEnvelopeMethods = {find(){return new Envelope(1,"M", 8, 1)}}
+        Dal.getEnvelopeData  = jest.fn().mockReturnValue(fakeEnvelopeMethods);
+        
+        const fakeStackMethods = {find(){return new CorticalStack(1,"M", "Toto", 8, 1)}}
+        Dal.getStackData = jest.fn().mockReturnValue(fakeStackMethods);
+
+        Dal.saveStackData = jest.fn()
+        Dal.removeEnvelopeData = jest.fn()
 
         weiClinic.killEnvelope(1)
 
-        expect(weiClinic.envelopes).toEqual([])
-        expect(weiClinic.stacks[0].idEnvelope).toBeNull()
+        expect(Dal.getStackData).toHaveBeenCalledTimes(1)
+        expect(Dal.getEnvelopeData).toHaveBeenCalledTimes(1)
+        expect(Dal.saveStackData).toHaveBeenCalledTimes(1)
+        expect(Dal.removeEnvelopeData).toHaveBeenCalledTimes(1)
+
+        
     });
 
     it('The enveloppe is not finded', () => {
-        weiClinic.envelopes = []
+        const fakeEnvelopeMethods = {find(){return null}}
+        Dal.getEnvelopeData  = jest.fn().mockReturnValue(fakeEnvelopeMethods);
+        Dal.getStackData = jest.fn()
 
         expect(() => { weiClinic.killEnvelope(3) }).toThrow("kil")
     });
