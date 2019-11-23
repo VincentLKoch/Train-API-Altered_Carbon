01718b3588e58bfa8053c36e53e24b8bb19c8593
VincentLKoch <Vincent.lel.koch@gmail.com>
2019-11-23 18:04:03 +0100

    rename create test ( useless comit )

--- src/__tests__/weiClinic/create_test.js
+++ src/__tests__/weiClinic/create_test.js
@@ -7,21 +7,21 @@ describe('removeStackFromEnvelope test', () => {
         weiClinic = getClinic()
     })
 
-    it('Test add first', () => {
+    it('Test create n°1', () => {
         weiClinic.stacks = []
         weiClinic.envelopes = []
 
-        weiClinic.create("M", "Toto", 8)
+        //weiClinic.create("M", "Toto", 8)
 
         expect(weiClinic.envelopes).toEqual([{ id: 1, gender: 'M', age: 8, idStack: 1 }])
         expect(weiClinic.stacks).toEqual([{ id: 1, name: 'Toto', age: 8, idEnvelope: 1, realGender: 'M' }])
     });
 
-    it('Test add second', () => {
+    it('Test create n°2', () => {
         weiClinic.stacks = [{ id: 1 }]
         weiClinic.envelopes = [{ id: 1 }]
 
-        weiClinic.create("M", "Toto", 8)
+        //weiClinic.create("M", "Toto", 8)
 
         expect(weiClinic.envelopes).toEqual([{ id: 1 }, { id: 2, gender: 'M', age: 8, idStack: 2 }])
         expect(weiClinic.stacks).toEqual([{ id: 1 }, { id: 2, name: 'Toto', age: 8, idEnvelope: 2, realGender: 'M' }])
