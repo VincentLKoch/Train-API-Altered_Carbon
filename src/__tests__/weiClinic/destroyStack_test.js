33456b4950f23854b257905af863dfd038f9c55c
Aadhar-Ag <55687136+Aadhar-Ag@users.noreply.github.com>
2019-11-23 22:12:49 +0100

    Update destroyStack_test.js

--- src/__tests__/weiClinic/destroyStack_test.js
+++ src/__tests__/weiClinic/destroyStack_test.js
@@ -13,40 +13,37 @@ describe('Destroy Stack test', () => {
 
     })
 
-    it('The stack is destroyed', () => {
-        const fakeStackMethods = {find(){return new CorticalStack(1,"M", "Toto", 8, null)}}
-        Dal.getStackData  = jest.fn().mockReturnValue(fakeStackMethods);
+    it('The stack is destroyed', async () => {
+        weiClinic.dal.getStackById = jest.fn()
+            .mockReturnValue({ id: 1, idEnvelope: null });
+        weiClinic.dal.removeStackData = jest.fn()
 
-        Dal.removeStackData  = jest.fn()
+        await weiClinic.destroyStack(1)
 
-        weiClinic.destroyStack(1)
-
-        expect(Dal.getStackData).toHaveBeenCalledTimes(1)
-        expect(Dal.removeStackData).toHaveBeenCalledTimes(1)
+        expect(weiClinic.dal.getStackById).toHaveBeenCalledWith(1)
+        expect(weiClinic.dal.removeStackData).toHaveBeenCalledWith(1)
 
     });
 
-    it('The stack and the envelope are destroyed', () => {
-        const fakeStackMethods = {find(){return new CorticalStack(1,"M", "Toto", 8, 1)}}
-        Dal.getStackData  = jest.fn().mockReturnValue(fakeStackMethods);
+    it('The stack and the envelope are destroyed',async () => {
+        weiClinic.dal.getStackById = jest.fn()
+            .mockReturnValue({ id: 1, idEnvelope: 3 });
+        weiClinic.dal.removeStackData = jest.fn()
+        weiClinic.dal.removeEnvelopeData = jest.fn()
 
-        Dal.removeStackData  = jest.fn()
-        Dal.removeEnvelopeData = jest.fn()
+        await weiClinic.destroyStack(1)
 
-        weiClinic.destroyStack(1)
-
-        expect(Dal.getStackData).toHaveBeenCalledTimes(1)
-        expect(Dal.removeStackData).toHaveBeenCalledTimes(1)
-        expect(Dal.removeEnvelopeData).toHaveBeenCalledTimes(1)
-        
+        expect(weiClinic.dal.getStackById).toHaveBeenCalledWith(1)
+        expect(weiClinic.dal.removeStackData).toHaveBeenCalledWith(1)
+        expect(weiClinic.dal.removeEnvelopeData).toHaveBeenCalledWith(3)
     });
 
-    it('The stack is not found', () => {
-        const fakeStackMethods = {find(){return null}}
-        Dal.getStackData  = jest.fn().mockReturnValue(fakeStackMethods);
+    it('The stack is not found', async () => {
+        weiClinic.dal.getStackById = jest.fn()
+        .mockReturnValue(null);
 
-        expect(() => { weiClinic.destroyStack(3) }).toThrow("ds")
-        expect(Dal.getStackData).toHaveBeenCalledTimes(1)
+        expect(() => { await weiClinic.destroyStack(3) }).toThrow("ds")
+        expect(weiClinic.dal.getStackById).toHaveBeenCalledTimes(1)
     });
 
 })
